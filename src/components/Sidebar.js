e38fcf3e995eea5377c2f3a8f3fbd4165a3ce0ef
Kimbogyung <novo4756@gmail.com>
2025-06-11 14:46:14 +0900

    feat: 사이드바 표시 기능

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <div className="sidebar">
       <div className="logo-container">
@@ -13,23 +16,23 @@ const Sidebar = () => {
       <div className="menu-header">HOSPITAL</div>
       
       <ul className="menu-list">
-        <li>
+        <li className={currentPath === '/info' ? 'active' : ''}>
           <span className="icon user-icon"></span>
           <Link to="/info">환자정보</Link>
         </li>
-        <li>
+        <li className={currentPath === '/history' ? 'active' : ''}>
           <span className="icon history-icon"></span>
           <Link to="/history">병력 / 주요 증상</Link>
         </li>
-        <li>
+        <li className={currentPath === '/diagnosis' ? 'active' : ''}>
           <span className="icon diagnosis-icon"></span>
           <Link to="/diagnosis">진단 내용</Link>
         </li>
-        <li>
+        <li className={currentPath === '/program' ? 'active' : ''}>
           <span className="icon treatment-icon"></span>
           <Link to="/program">이전 치료 프로그램</Link>
         </li>
-        <li>
+        <li className={currentPath === '/recommend' ? 'active' : ''}>
           <span className="icon recommendation-icon"></span>
           <Link to="/recommend">유사 환자 기반 치료 추천</Link>
         </li>
